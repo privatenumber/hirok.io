ce6bda0a9c044e05d199cb3117fa0fa4c6ae5793
Hiroki Osame <hiroki.osame@gmail.com>
2022-08-06 04:06:08 -0400

    chore: upgrade vite plugins

--- src/@types/unplugin-vue-components.d.ts
+++ src/@types/unplugin-vue-components.d.ts
@@ -3,6 +3,8 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
+export {}
+
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Button: typeof import('./../components/Button.vue')['default']
@@ -25,5 +27,3 @@ declare module '@vue/runtime-core' {
     WiggleAnimation: typeof import('./../components/WiggleAnimation.vue')['default']
   }
 }
-
-export {}
