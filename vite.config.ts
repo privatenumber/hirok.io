29dce04bd70eb036643de13b686ac9c5a850ebf4
Hiroki Osame <hiroki.osame@gmail.com>
2022-08-06 03:45:26 -0400

    chore: upgrade eslint

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ import pages from 'vite-plugin-pages';
 import unocss from 'unocss/vite';
 import { presetUno } from 'unocss';
 import transformerDirective from '@unocss/transformer-directives';
-import autoImport from 'unplugin-auto-import/vite'; // eslint-disable-line import/no-unresolved
-import components from 'unplugin-vue-components/vite'; // eslint-disable-line import/no-unresolved
+import autoImport from 'unplugin-auto-import/vite';
+import components from 'unplugin-vue-components/vite';
 import icons from 'unplugin-icons/vite';
 import iconsResolver from 'unplugin-icons/resolver';
 import grayMatter from 'gray-matter';
