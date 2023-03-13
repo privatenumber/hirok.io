5c5f75345dcea6c9af5b42baa259c020649b8f1d
Hiroki Osame <hiroki.osame@gmail.com>
2023-03-13 11:27:06 +0900

    fetch multiple months of npm dls

--- scripts/get-npm-downloads.ts
+++ scripts/get-npm-downloads.ts
@@ -1,7 +1,42 @@
 import fs from 'fs/promises';
+import { setTimeout } from 'timers/promises';
+import { cli } from 'cleye';
 import { search, downloads } from '@nodesecure/npm-registry-sdk';
 
-const getMonthFirstDay = () => {
+type NpmPackage = {
+	name: string;
+	repository: string | undefined;
+	description: string;
+	lastPublishDate: string;
+	latestVersion: string;
+	downloads: Record<string, number>;
+};
+
+type LastMonth = [month: string, downloads: number];
+
+type NpmDownloads = {
+	packages: NpmPackage[];
+	lastMonth: LastMonth;
+	fetched: string;
+};
+
+const npmDownloadsJsonPath = './src/data/npm-downloads.json';
+
+const getNpmDownloadsJson = async (): Promise<NpmDownloads> => {
+	const fileExists = await fs.access(npmDownloadsJsonPath).then(() => true, () => false);
+	if (!fileExists) {
+		return {
+			packages: [],
+			lastMonth: ['', 0],
+			fetched: '',
+		};
+	}
+
+	const npmDownloadsString = await fs.readFile(npmDownloadsJsonPath, 'utf8');
+	return JSON.parse(npmDownloadsString);
+};
+
+const getThisMonth = () => {
 	const date = new Date();
 	date.setUTCHours(0);
 	date.setUTCMinutes(0);
@@ -14,8 +49,8 @@ const getMonthFirstDay = () => {
 
 const getIsoDate = (date: Date) => date.toISOString().split('T')[0];
 
-const getLastMonthRange = () => {
-	const start = getMonthFirstDay();
+type MonthRange = [startDate: string, endDate: string];
+const getLastMonthRange = (start: Date): MonthRange => {
 	const end = new Date(start);
 	end.setMonth(end.getMonth() + 1);
 	end.setDate(0);
@@ -23,46 +58,89 @@ const getLastMonthRange = () => {
 	return [
 		getIsoDate(start),
 		getIsoDate(end),
-	] as [startDate: string, endDate: string];
+	];
 };
 
-(async () => {
+const getNpmPackages = async () => {
+	const npmDownloads = await getNpmDownloadsJson();
+
 	const results = await search({
 		text: 'author:hirokiosame',
 		size: 250,
 	});
 
-	// eg. 2023-01-01:2023-01-31
-	const lastMonthRange = getLastMonthRange();
+	const npmPackages = results.objects.map(({ package: npmPackage }): NpmPackage => {
+		const previousData = npmDownloads.packages.find(p => p.name === npmPackage.name);
 
-	const packages = await Promise.all(
-		results.objects.map(
-			async ({ package: npmPackage }) => {
-				const downloadCount = await downloads(npmPackage.name, lastMonthRange.join(':'));
-
-				return {
-					name: npmPackage.name,
-					repository: npmPackage.links.repository,
-					description: npmPackage.description,
-					lastPublishDate: npmPackage.date,
-					latestVersion: npmPackage.version,
-					downloads: downloadCount.downloads,
-				};
+		return {
+			name: npmPackage.name,
+			repository: npmPackage.links.repository,
+			description: npmPackage.description,
+			lastPublishDate: npmPackage.date,
+			latestVersion: npmPackage.version,
+			downloads: {
+				...previousData?.downloads,
 			},
-		),
-	);
+		};
+	});
 
-	packages.sort((a, b) => a.name.localeCompare(b.name));
+	npmPackages.sort((a, b) => a.name.localeCompare(b.name));
 
-	const npmPackages = {
+	return npmPackages;
+};
+
+const getDownloadsForMonth = async (
+	packages: NpmPackage[],
+	monthRange: MonthRange,
+) => {
+	const monthRangeString = monthRange.join(':');
+	const monthDownloads = await Promise.all(packages.map(async (npmPackage) => {
+		const downloadCount = await downloads(npmPackage.name, monthRangeString);
+		npmPackage.downloads[monthRange[0]] = downloadCount.downloads;
+		return downloadCount.downloads;
+	}));
+
+	return monthDownloads.reduce((total, d) => total + d, 0);
+};
+
+(async () => {
+	const argv = cli({
+		flags: {
+			range: {
+				type: Number,
+				alias: 'm',
+				description: 'Number of months to go back to fetch',
+				default: 1,
+			},
+		},
+	});
+
+	const packages = await getNpmPackages();
+	const thisMonth = getThisMonth();
+
+	let lastMonth: LastMonth;
+	for (let i = argv.flags.range - 1; i >= 0; i -= 1) {
+		const month = new Date(thisMonth);
+		month.setMonth(month.getMonth() - i);
+		const monthRange = getLastMonthRange(month);
+
+		const lastMonthDownloads = await getDownloadsForMonth(packages, monthRange);
+
+		if (i === 0) {
+			lastMonth = [monthRange[0], lastMonthDownloads];
+		}
+
+		await setTimeout(1000);
+	}
+
+	const npmDownloads: NpmDownloads = {
 		packages,
-		totalDownloads: packages.reduce((total, p) => total + p.downloads, 0),
-		totalDownloadsMonth: lastMonthRange[0],
-		fetched: results.time,
+		lastMonth: lastMonth!,
+		fetched: new Date().toISOString(),
 	};
 
 	await fs.writeFile(
-		'./src/data/npm-downloads.json',
-		`${JSON.stringify(npmPackages, null, '\t')}\n`,
+		npmDownloadsJsonPath,
+		`${JSON.stringify(npmDownloads, null, '\t')}\n`,
 	);
 })();
